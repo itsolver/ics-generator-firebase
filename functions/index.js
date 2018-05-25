793c614ddcf705e7a6a6d25bf722e7f6045b87f8
itsolver <26458299+itsolver@users.noreply.github.com>
2018-05-25 20:36:12 +1000

    working now

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,31 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var underscore = require('underscore');
+const express = require('express');
+var templateString = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//<%= host %><% if(name) { %>
+X-WR-CALNAME:<%= name %><% } %>
+X-WR-TIMEZONE:<%= timezone %>
+BEGIN:VEVENT
+DTSTAMP:<%= now %>
+UID:<%= uid %><% if (allDay) { %>
+DTSTART;VALUE=DATE:<%= startDate %>
+DTEND;VALUE=DATE:<%= endDate %><% } else { %>
+DTSTART;TZID=Etc/GMT:<%= startDate %>
+DTEND;TZID=Etc/GMT:<%= endDate %><% } %>
+SUMMARY:<%= summary %>
+DESCRIPTION:<%= description %>
+LOCATION:<%= location %><% if(rrule) { %>
+RRULE:<%= rrule %><% } %>
+END:VEVENT
+END:VCALENDAR`;
+const host = 'ics.itsolver.net';
 
-var template = underscore.template(fs.readFileSync('./ics.template').toString());
+var template = underscore.template(templateString);
 
-var port = process.env['PORT'] || 443;
-var host = process.env['HOST'] || 'ics.itsolver.net';
-
-
-
-http.createServer(function(req, res) {
+const app = express();
+app.get('*', (req, res) => {
   var params = url.parse(req.url, true).query;
 
   var options = {
@@ -22,6 +38,7 @@ http.createServer(function(req, res) {
     location: params.location,
     name: params.name,
     allDay: params.all_day,
+    fileName: params.file_name,
     rrule: params.rrule
   };
 
@@ -35,14 +52,20 @@ http.createServer(function(req, res) {
 
   options.uid = (new Date()).getTime() + "@" + host;
   options.now = formatDate(new Date());
+  options.fileName = sanitizeFileName(options.fileName || '');
+  if (!options.fileName.length) {
+    options.fileName = 'Event';
+  }
 
   var output = template(options);
 
-  res.writeHead(200, {'Content-Type': 'text/calendar'});
+  res.writeHead(200, {
+    'Content-Type': 'text/calendar',
+    'Content-Disposition': `attachment; filename="${options.fileName}.ics"`
+  });
   res.end(output);
-}).listen(port);
-
-console.log("Listening on port " + port + ", host " + host);
+});
+exports.app = functions.https.onRequest(app);
 
 function formatDatetime(d) {
   return d.getFullYear() + pad2(d.getMonth() + 1) + pad2(d.getDate()) + "T" + pad2(d.getHours()) + pad2(d.getMinutes()) + pad2(d.getSeconds());
@@ -52,12 +75,14 @@ function formatDate(d) {
   return d.getFullYear() + pad2(d.getMonth() + 1) + pad2(d.getDate());
 }
 
+function sanitizeFileName(filename) {
+  return filename.replace(/[^A-Za-z0-9_\-.]/g, ()=>'');
+}
+
 function pad2(i) {
   if(i < 10) {
     return "0" + i;
   } else {
-    return "" + i;
+    return String(i);
   }
-};
-
-export const app = functions.https.onRequest(app);
+}
